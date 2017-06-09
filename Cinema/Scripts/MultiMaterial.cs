2615adadf36a0a64e098a297da50202a78527123
jonathan <jonathann@unity3d.com>
2017-06-08 18:04:37 -0700

    add ability to save mulitmaterialdata from renderer

--- Cinema/Scripts/MultiMaterial.cs
+++ Cinema/Scripts/MultiMaterial.cs
@@ -2,18 +2,30 @@
 
 namespace UnityLabs.Cinema
 {
+    [RequireComponent(typeof(Renderer))]
     public class MultiMaterial : MonoBehaviour
     {
         [SerializeField]
         MultiMaterialData m_MultiMaterialData;
+
+        MaterialArray m_MaterialArray;
 
         public MultiMaterialData multiMaterialData
         {
             get { return m_MultiMaterialData; }
         }
 
+        public MaterialArray materialArray
+        {
+            get { return m_MultiMaterialData != null ? m_MultiMaterialData.materialArrayData : m_MaterialArray; }
+#if UNITY_EDITOR
+            set { m_MaterialArray = value; }
+#endif
+        }
+
 #if UNITY_EDITOR
         public const string multiMaterialPub = "m_MultiMaterialData";
+        public const string materialArrayPub = "m_MaterialArray";
 #endif
     }
 }
